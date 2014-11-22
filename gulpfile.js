882fcfb3ab939334a496bf8733eecbc29cd4db71
scippio <scippio@2ants.eu>
2014-11-22 21:24:46 +0100

    add *.js files for code climate and add docco into gulp

--- gulpfile.js
+++ gulpfile.js
@@ -21,17 +21,17 @@ var paths = {
 gulp.task('compile-scripts', function () {
     return gulp.src(paths.scripts.in)
             .pipe(coffee())
-            .pipe(gulp.dest(paths.scripts.out));
-//            .pipe(docco())
-//            .pipe(gulp.dest(paths.docs.out));
+            .pipe(gulp.dest(paths.scripts.out))
+            .pipe(docco())
+            .pipe(gulp.dest(paths.docs.out));
 });
 
 gulp.task('compile-tests', function () {
     return gulp.src(paths.tests.in)
             .pipe(coffee())
-            .pipe(gulp.dest(paths.tests.out));
-//            .pipe(docco())
-//            .pipe(gulp.dest(paths.docs.out));
+            .pipe(gulp.dest(paths.tests.out))
+            .pipe(docco())
+            .pipe(gulp.dest(paths.docs.out));
 });
 
 gulp.task('tests', ['default'], function () {
